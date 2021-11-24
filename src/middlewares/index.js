c9cd0951f3ce69fb26940c547f2b3394936b5fc3
Camila <camilabentespessoa@gmail.com.br>
2021-11-24 17:24:59 -0300

    add auth middleware

---
+++ src/middlewares/index.js
@@ -0,0 +1,19 @@
+const middlewares = {
+
+    authenticate(req, res, next){
+
+        const { authentication } = req.headers
+        const { user_id } = req.params
+
+        if (!authentication) return res.status(400).json({message: 'No token'})
+
+        if(authentication !== user_id) return res.status(400).json({message: 'Permission Denied'})
+
+        next()
+    }
+
+    }
+
+
+
+module.exports = middlewares
